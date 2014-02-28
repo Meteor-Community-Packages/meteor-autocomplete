939d50d740a1ab06136c262d22783242b44dca9e
Andrew Mao <mizzao@gmail.com>
2014-02-28 12:40:08 -0500

    pull in jquery-migrate for 0.7.1.2

--- package.js
+++ package.js
@@ -1,18 +1,20 @@
 Package.describe({
-    summary: "symbol-triggered autocompletion using Meteor collections"
+  summary: "symbol-triggered autocompletion using Meteor collections"
 });
 
 Package.on_use(function (api) {
-    api.use(['handlebars', 'templating'], 'client');
-    api.use(['coffeescript', 'jquery'], 'client');
+  api.use(['handlebars', 'templating'], 'client');
+  api.use(['coffeescript', 'jquery'], 'client');
 
-    // Our files
-    api.add_files([
-        'autocomplete.css',
-        'inputs.html',
-        'jquery.caretposition.js',
-        'autocomplete.coffee',
-        'templates.coffee'
-    ], 'client');
+  // Required by caretposition.js right now
+  api.use('jquery-migrate');
 
+  // Our files
+  api.add_files([
+      'autocomplete.css',
+      'inputs.html',
+      'jquery.caretposition.js',
+      'autocomplete.coffee',
+      'templates.coffee'
+  ], 'client');
 });
