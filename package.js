3b6045b0a5dcfa668e07d1bb00662c516f8a76eb
Andrew Mao <mizzao@gmail.com>
2014-09-08 02:17:20 -0400

    0.4.8 release

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "mizzao:autocomplete",
   summary: "Client/server autocompletion designed for Meteor's collections and reactivity",
-  version: "0.4.7",
+  version: "0.4.8",
   git: "https://github.com/mizzao/meteor-autocomplete.git"
 });
 
